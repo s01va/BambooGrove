5e55e02174e17a3ceb17be7df0b8c5bce3102055
s01va <jinn0525@gmail.com>
2021-08-25 23:46:17 +0900

    add user model

--- model/User.js
+++ model/User.js
@@ -1,1 +1,12 @@
 //const crypto = require('crypto')
+
+class User {
+    constructor(data) {
+        this.username = data.username;
+        this.password = data.password;
+        this.name = data.name;
+        this.email = data.email;
+    }
+}
+
+module.exports = (data) => new insertUser(data);
